d680abbf2746e7d17c80a420e90b9243dec140e6
tomek9409 <kolpak9409@gmail.com>
2022-09-22 17:29:33 +0200

    fadeOut

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".hamburger").click(function () {
+    $(".main-menu").toggleClass("open");
+  });
+  $(".main-menu-item a").click(function (e) {
+    e.preventDefault();
+    $("body").fadeOut(2000, function () {
+      window.location.href = $(e.target).attr("href");
+    });
+  });
+});
